9d3292134bb976321b64825c83f69640b5b2158a
tiago-forward <devtiagoforward@gmail.com>
2025-08-06 21:23:58 -0300

    feat: authentication and home screen

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fsc-projects-static.s3.us-east-1.amazonaws.com", // Certifique-se de que está aqui
+      },
+      {
+        protocol: "https",
+        hostname: "d4lgxe9bm8juw.cloudfront.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
